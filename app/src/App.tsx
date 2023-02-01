eea929b8bc94f66b4a02a8ed9ae23e4b648fad99
VladIstratescu08 <vlad.istratescu@bitsoftware.ro>
2023-02-01 14:01:28 +0200

    add navbar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css'
 import Nav from "./components/Nav";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <Nav></Nav>
+      <div className="row">
+        <Nav></Nav>
+        <Home></Home>
+      </div>
   );
 }
 
---
+++ app/src/components/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Home.css'
+
+function Home(){
+    return (
+        <div className="container">
+            <div className="row center">
+                <div className="col s12">
+                    <a href="#" className="btn yellow darken-3 waves-effect">Welcome</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
